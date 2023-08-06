29e0721d8d40e939582051be081b04f86363d51e
rzk2l <rezkellah.rabah@gmail.com>
2023-08-06 01:26:30 +0100

    Finished Get started

---
+++ lib/colors/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  Color bg = Color.fromARGB(255, 223, 223, 223);
+  Color gsbg1 = Color.fromARGB(255, 40, 40, 40);
+  Color gsbg2 = Color.fromARGB(255, 64, 64, 64);
+  Color itembg = Color.fromARGB(255, 195, 188, 185);
+  Color fnt = Color.fromARGB(255, 11, 11, 11);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,80 @@
+import 'package:first_attempt/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'colors/colors.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(initialRoute: '/', routes: {
-    '/': (context) => HomeScreen(),
+    '/': (context) => GetStarted(),
+    '/home': (context) => HomeScreen(),
   }));
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class GetStarted extends StatelessWidget {
+  GetStarted({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  final myColorsInstance = MyColors();
 
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [myColorsInstance.gsbg1, myColorsInstance.gsbg2],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+            stops: [0.7, 1],
+            transform: GradientRotation(pi / 4),
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image(
+                  image: NetworkImage(
+                "https://static.thenounproject.com/png/2635332-200.png",
+              )),
+              SizedBox(height: 30),
+              Text(
+                "Let's improve your style",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: myColorsInstance.bg,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 15),
+              Text(
+                "Find cool shoes to support your daily activities",
+                style: TextStyle(
+                    color: myColorsInstance.bg, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(myColorsInstance.bg),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14)))),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 45, vertical: 18),
+                  child: Text(
+                    "Get Started",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: myColorsInstance.fnt,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 60),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
