870d753c4555a6a28a22c7cbb13f481765aa0fdd
rzk2l <rezkellah.rabah@gmail.com>
2023-08-15 17:13:08 +0100

    Finished working on favorites

--- lib/pages/favs.dart
+++ lib/pages/favs.dart
@@ -1,27 +1,132 @@
 import 'package:first_attempt/controllers/favorites_controller.dart';
+import 'package:first_attempt/services/colors.dart';
+import 'package:first_attempt/services/items.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class FavoritesScreen extends StatelessWidget {
-  const FavoritesScreen({super.key});
+  FavoritesScreen({super.key});
+  final FavoritesController controller = Get.put(FavoritesController());
+  final MyColors myColorsInstance = MyColors();
 
   @override
   Widget build(BuildContext context) {
-    var controller = Get.put(FavoritesController());
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Favorites"),
-        centerTitle: true,
-      ),
-      body: Obx(
-        () => ListView.builder(
-            itemCount: controller.favItems.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text('${controller.favItems[index]}'),
-              );
-            }),
-      ),
-    );
+        backgroundColor: myColorsInstance.bg,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(25, 20, 25, 0),
+            child: Column(
+              children: [
+                SizedBox(height: 25),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Your Favorites",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Obx(
+                  () => GridView.builder(
+                    physics: ScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: controller.favItems.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      mainAxisExtent: 215,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                                width: double.infinity,
+                                height: 150,
+                                color: myColorsInstance.itembg,
+                                child: Image.asset(
+                                  controller.favItems[index].imageUrl,
+                                  fit: BoxFit.fitWidth,
+                                )),
+                            SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      controller.favItems[index].price,
+                                      style: TextStyle(
+                                        fontSize: 19,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(
+                                      controller.favItems[index].name,
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.w600),
+                                    )
+                                  ],
+                                ),
+                                Expanded(
+                                  child: SizedBox(),
+                                ),
+                                Obx(() => IconButton(
+                                    onPressed: () {
+                                      if (controller.favItems[index].isFav) {
+                                        GridItem favItem =
+                                            controller.favItems[index];
+                                        favItem.isFav = false;
+                                        controller.favItems[index] = favItem;
+                                        controller.favItems
+                                            .remove(controller.favItems[index]);
+                                        GridItem item = controller.items[index];
+                                        item.isFav = false;
+                                        controller.items[index] = item;
+                                      } else {
+                                        GridItem favItem =
+                                            controller.favItems[index];
+                                        favItem.isFav = true;
+                                        controller.favItems[index] = favItem;
+                                        controller.favItems
+                                            .add(controller.favItems[index]);
+                                        GridItem item = controller.items[index];
+                                        item.isFav = true;
+                                        controller.items[index] = item;
+                                      }
+                                    },
+                                    icon: (controller.favItems[index].isFav)
+                                        ? Icon(
+                                            Icons.favorite,
+                                            color: Colors.red,
+                                          )
+                                        : Icon(
+                                            Icons.favorite,
+                                            color: Colors.red,
+                                          ))),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
